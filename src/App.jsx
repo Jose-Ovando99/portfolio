7c047843ccb5cb0a3ba061d946f6bd088cecca0d
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-06-12 16:57:01 -0600

    inicalizar proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+
+function App() {
+  
+  return (
+    <>
+      <Header me='Ovando'/>
+      <p className="text-red-500 text-2xl bg-slate-400 rounded-md">About me</p>
+      <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Skills</p>
+      <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Some projects</p>
+      <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Contacme</p>
+      <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Footer</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header({ me }) {
+  return (
+    <div>
+
+      <h1 className="text-2xl">{`Hello world by ${me}`}</h1>
+    </div>
+  )
+}
+
+export default Header;

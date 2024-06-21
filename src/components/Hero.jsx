b7c9efdac6d5683b707ca7be579357dd9961b6ef
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-06-20 19:13:51 -0600

    agregar switch de fotos a Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { useState, React } from 'react';
+import pruebaUno from '../assets/pruebaUno.jpg';
+import pruebaDos from '../assets/pruebaDos.png';
 
 function Hero() {
-  const [valor, setValor] = useState(0)
-  
+
+  const [fotoPerfil, setFotoPerfil] = useState(pruebaUno)
+
+  let cambiarFoto = () => {
+    (fotoPerfil == pruebaUno) ? setFotoPerfil(pruebaDos) : setFotoPerfil(pruebaUno);
+  };
+
   return (
-    <div>
-    <div className="text-slate-400">El contador es {valor}</div>
-    <button onClick={() => setValor(valor + 1)}>Dame click!:D</button>
-    <button onClick={() => setValor(0)}>volvamos a cero</button>
+    <div className='flex justify-center items-center flex-col md:flex-row-reverse bg-slate-300'>
+      <div className='text-center font-mono text-3xl justify-center items-center'>
+        <p>Hola, soy <span className='font-semibold'>Ivan Ovando</span><br />
+        Desarrollador Front-end</p>
+      </div>
+      <div className='flex flex-col justify-center items-center'>
+        <img className='rounded-full w-[280px] h-[280px] object-center object-cover'
+          src={fotoPerfil} alt='Img de prueba' onClick={() => cambiarFoto()} />
+        <p className='font-mono text-lg'>Hazme click!</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero;

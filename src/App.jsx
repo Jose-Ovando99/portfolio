aeb94ab573846dd370e986fa863ecc5cf763f338
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-06-23 12:43:10 -0600

    agregar textos para cambio de idioma

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import { LanguageProvider } from "./components/LanguageContext";
 
 
 function App() {
   
   return (
-    <>
+    <LanguageProvider>
       <Header />
       <Hero />
       <p className="text-red-500 text-2xl bg-slate-400 rounded-md">About me</p>
@@ -13,7 +14,7 @@ function App() {
       <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Some projects</p>
       <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Contacme</p>
       <p className="text-red-500 text-2xl bg-slate-400 rounded-md">Footer</p>
-    </>
+    </LanguageProvider>
   )
 }
 
---
+++ src/constants/textos.js
@@ -0,0 +1,14 @@
+const textos = {
+  es: {
+    saludo: 'Hola, soy ',
+    puesto: 'Desarrollador Front-end',
+    click: 'Hazme click',
+  },
+  en: {
+    saludo: 'Hi, I\'m ',
+    puesto: 'Front-end developer',
+    click: 'Click me',
+  }
+};
+
+export default textos;

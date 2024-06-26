8ff182dfb134e49170f67479875ee0d1de250b32
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-06-25 21:27:18 -0600

    actualizar textos y Hero.jsx

--- src/constants/textos.js
+++ src/constants/textos.js
@@ -1,13 +1,17 @@
 const textos = {
   es: {
-    saludo: 'Hola, soy ',
-    puesto: 'Desarrollador Front-end',
+    saludo: 'Hola, mi nombre es ',
+    puesto: 'Hago sitios web',
     click: 'Hazme click',
+    saludoHero: 'Gusto en conocerte.',
+    textoSobreMi: 'Regresemos al 2023, un año en el que atravesaba una etapa emocionalmente complicada. Por casualidad, YouTube me recomendó un video sobre HTML. Aunque solo duraba unos 10 minutos, fue suficiente para captar toda mi atención y despertar una curiosidad que pronto se convirtió en pasión por el desarrollo web. Retomé la programación, la cual había dejado totalmente olvidada desde 2016. Programar en el front end es algo que realmente disfruto y me apasiona profundamente. Ha llegado a ser, incluso, terapéutico para mí.',
   },
   en: {
-    saludo: 'Hi, I\'m ',
-    puesto: 'Front-end developer',
+    saludo: 'Hi, my name is ',
+    puesto: 'I make web sities',
     click: 'Click me',
+    saludoHero: 'Nice to meet you.',
+    textoSobreMi: 'Let\'s go back to 2023, a year when I was going through an emotionally complicated stage.By chance, YouTube recommended a video about HTML to me.Although it only lasted about 10 minutes, it was enough to capture all of my attention and awaken a curiosity that soon evolved into a passion for web development.I picked up programming again, which I had completely forgotten since 2016. Programming on the front end is something I truly enjoy and am deeply passionate about.It has become, to a certain extent, therapeutic for me.',
   }
 };
 

9537f77702aed6be4a6192ffd0ade5ee62fc333f
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-07-20 18:15:28 -0600

    agregar imagenes en componente src/components/skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import textos from '../constants/textos';
 import { useLanguage } from './LanguageContext';
 import { IconBrandFigma, IconBrandSketch, IconFilePencil, IconBrandHtml5, IconBrandCss3, IconBrandJavascript, IconBrandPython, IconBrandGit, IconBrandNodejs, IconBrandReact, IconBrandTailwind, IconBrandBootstrap, IconBrandGithub, IconBrandVscode } from '@tabler/icons-react';
+import diseniadorimg from '../assets/personajeFigma-export.png';
+import frontimg from '../assets/personajeJS-export.png';
 
 function Skills() {
   const { idioma } = useLanguage();
   const { diseniador, diseniadorTexto, frontEnd, frontEndTexto, herramientasDiseniador, herramientasDesarrollador,lapizYPapel } = textos[idioma];
 
   return (
-    <div className="text-2xl font-nunito text-slate-900 bg-slate-200 flex flex-col md:flex-row px-12 text-center justify-center pb-20 dark:bg-slate-900">
-      <div className='border-2 md:border-r rounded-xl rounded-b-none border-gray-500 bg-slate-200 -mt-40 p-8 md:rounded-xl md:w-2/5 dark:border-slate-200 dark:bg-slate-900 dark:text-slate-200'>
+    <div className="text-2xl font-nunito text-slate-900 bg-slate-200 flex flex-col md:flex-row px-12 text-center justify-center pb-20 dark:bg-slate-900 select-none">
+      <div className='border-2 md:border-r rounded-xl rounded-b-none border-gray-500 bg-slate-200 -mt-40 p-8 md:rounded-xl md:w-2/5 dark:border-slate-200 dark:bg-slate-900 dark:text-slate-200 flex items-center flex-col'>
+        <img src={diseniadorimg} className='w-2/3 max-w-52' />
         <p className='font-extrabold text-amber-600'>{diseniador}</p>
         <p className='font-semibold py-5'>{diseniadorTexto}</p>
         <p className='py-5'>{herramientasDiseniador}</p>
@@ -19,7 +22,8 @@ function Skills() {
           <li className='flex items-center justify-center my-2'><IconFilePencil />{lapizYPapel}</li>
         </ul>
       </div>
-      <div className='border-2 md:border-l rounded-xl rounded-t-none border-gray-500 bg-slate-200 -mt-1 md:-mt-40 md:rounded-xl p-8 md:w-2/5 dark:border-slate-200 dark:bg-slate-900 dark:text-slate-200'>
+      <div className='border-2 md:border-l rounded-xl rounded-t-none border-gray-500 bg-slate-200 -mt-1 md:-mt-40 md:rounded-xl p-8 md:w-2/5 dark:border-slate-200 dark:bg-slate-900 dark:text-slate-200 flex items-center flex-col'>
+        <img src={frontimg} className='w-2/3 max-w-52' />
         <p className='font-extrabold text-amber-600'>{frontEnd}</p>
         <p className='font-semibold py-5'>{frontEndTexto}</p>
         <p className='py-5'>{herramientasDesarrollador}</p>

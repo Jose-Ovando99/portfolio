015cade2932f7aac4654acf9ea0c87a7f853437d
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-07-29 15:41:05 -0600

    agregar logica y maquetado a componente CardProject.jsx, mismo componente se renderiza en Projects.jsx

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,1 +1,38 @@
 // Hacer para reutilizar en projects.jscx
+import React from 'react';
+import { useLanguage } from './LanguageContext';
+import textos from '../constants/textos';
+import { IconAppWindow, IconCode } from '@tabler/icons-react';
+
+function CardProject({ title, imageURL, textAbout, page, code, classAdd }) {
+  const { idioma } = useLanguage();
+  const { pagina, codigo } = textos[idioma]
+  const textoSobre = textos[idioma][textAbout];
+
+  return (
+    <article className={`flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl bg-slate-200 px-16 py-8 mb-10 md:w-2/5 dark:bg-slate-900 dark:border-slate-200 ${classAdd}`}>
+      <p className='font-extrabold text-2xl dark:text-slate-200'>
+        {title}
+      </p>
+      {/* ruta require node react.js */}
+      <img className='w-36 rounded-full m-3' 
+      src={imageURL} alt={`Imagen de ${title}`} />
+      <p className='text-justify dark:text-slate-200' >
+        {textoSobre}
+      </p>
+      {/* Enlaces */}
+      <div className="flex flex-row justify-between items-center mt-4">
+        <button className='bg-sky-900 text-amber-600 border rounded-xl flex justify-center items-center m-2 w-1/2 leading-none h-10 font-semibold p-2'>
+          <IconAppWindow size={34} />
+          <a href={page} target='_blank'>{pagina}</a>
+        </button>
+        <button className='bg-sky-900 text-amber-600 border rounded-xl flex justify-center items-center m-2 w-1/2 leading-none h-10 font-semibold p-2'>
+          <IconCode size={34} />
+          <a href={code} target='_blank'>{codigo}</a>
+        </button>
+      </div>
+    </article>
+  )
+}
+
+export default CardProject;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
-import { useLanguage } from './LanguageContext';
-import textos from '../constants/textos';
-import { IconAppWindow, IconCode } from '@tabler/icons-react'
+import CardProject from './CardProject';
+
+//Importacion de imagenes para utilizar en el componente
 import moritaCover from '../assets/moritaCover.jpg';
 
 
 function Projects() {
-  const { idioma } = useLanguage();
-  const { pagina, codigo, trabajoMorita } = textos[idioma];
 
   return (
     <div className='font-nunito text-slate-900 bg-slate-200 flex flex-col md:flex-row px-12 text-center justify-center pb-24 dark:bg-slate-900'>
-      {/* movi el margin bottom para dar mas espacio en blanco el componente reutilizable debe ir aqui!!aqui empieza el componente*/}
-      <div className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl -mt-40 bg-slate-200 py-8 px-16 mb-10 md:w-2/5 md:mx-4 dark:border-slate-200 dark:bg-slate-900">
-        <p className='font-extrabold text-2xl dark:text-slate-200'>Morita Nail Studio</p>
-        <img className='w-36 rounded-full m-3' src={moritaCover} />
-        <p className='text-justify dark:text-slate-200'>{trabajoMorita}</p>
-        {/* Enlaces */}
-        <div className='flex flex-row justify-between items-center mt-4'>
-          <button className='bg-sky-900 text-amber-600 border rounded-xl flex justify-center items-center m-2 w-1/2 leading-none h-10 font-semibold p-2'>
-            <IconAppWindow size={34} />
-            <a href='https://google.com' target='_blank'>{pagina}</a>
-          </button>
-          <button className='bg-sky-900 text-amber-600 border rounded-xl flex justify-center items-center m-2 w-1/2 leading-none h-10 font-semibold p-2'>
-            <IconCode size={34} />
-            <a href='https://github.com' target='_blank'>{codigo}</a>
-          </button>
-        </div>
-        {/* Aqui termina el componente */}
-      </div>
-
+      <CardProject 
+      title='Morita Nail Studio'
+      imageURL={moritaCover}
+      textAbout='trabajoMorita'
+      page='https://google.com'
+      code='https://github.com'
+      classAdd='-mt-11 md:-mt-40'
+      />
     </div>
   );
 }

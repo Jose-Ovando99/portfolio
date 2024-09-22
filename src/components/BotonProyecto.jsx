22aa80cb13f5db588a28a12ca015f83029955013
Jose-Ovando99(Linux) <162391184+Jose-Ovando99@users.noreply.github.com>
2024-09-21 21:25:52 -0600

    agregar BotonProyecto.jsx y modificar constants/textos.js , falta estilizar la card y agregar imagenes

---
+++ src/components/BotonProyecto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BotonProyecto({ proyectoTitulo, proyectoInfo, handleCLick }) {
+  return (
+    <button onClick={() => handleCLick(proyectoInfo)}
+    className="border border-slate-900 bg-neutral-300 rounded-2xl p-2 hover:text-amber-600 md:w-1/4 w-5/12 md:h-12 h-20 focus:font-extrabold focus:text-amber-600 focus:bg-sky-900 dark:bg-slate-900 dark:border-slate-200">
+    <p>{proyectoTitulo}</p>
+  </button>
+  )
+}
+
+export default BotonProyecto;
--- src/components/MoreProjects.jsx
+++ src/components/MoreProjects.jsx
@@ -1,29 +1,57 @@
-import React from "react";
+import { useState } from "react";
 import textos from "../constants/textos";
 import { useLanguage } from "./LanguageContext";
+import BotonProyecto from "./BotonProyecto";
 
 function MoreProjects() {
   const { idioma } = useLanguage();
-  const { masProyectos } = textos[idioma];
+  const { masProyectos, proCro, proCit, proTai, proBoo, proTdj, proTdr, proCal } = textos[idioma];
+
+  const [proyectoRef, setProyectoRef] = useState("Cro");
+
+  let infoProyecto = {
+    proyectoNombre: textos[idioma][`pro${proyectoRef}`],
+    proyectoDescripcion: textos[idioma][`pro${proyectoRef}Des`],
+    proyectoDemo: textos[idioma][`pro${proyectoRef}Demo`],
+    proyectoTecnologias: textos[idioma][`pro${proyectoRef}Tec`]
+  };
+
+  const mostrarProyecto = (referencia) => {
+    setProyectoRef(referencia)
+  }
 
   return (
-    <section className="pb-40 -mt-20 font-nunito text-slate-900 bg-slate-200 dark:bg-slate-900 dark:text-slate-200">
-      <p className="text-center font-extrabold text-2xl">{masProyectos}</p>
-      <div className="flex flex-row justify-center items-center">
-        <p className="text-center">Boton1</p>
-        <p className="text-center">Boton2</p>
+
+    <section className="pb-40 -mt-20 font-nunito text-slate-900 bg-slate-200 dark:bg-slate-900 dark:text-slate-200 px-8 flex flex-col">
+      <p className="text-center font-extrabold text-2xl mx-8">{masProyectos}</p>
+      <div className="flex flex-row flex-wrap justify-center items-center gap-2 font-bold my-4">
+        {/* botones */}
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Cro" proyectoTitulo={proCro} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Cit" proyectoTitulo={proCit} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Tai" proyectoTitulo={proTai} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Boo" proyectoTitulo={proBoo} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Tdj" proyectoTitulo={proTdj} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Tdr" proyectoTitulo={proTdr} />
+        <BotonProyecto handleCLick={mostrarProyecto} proyectoInfo="Cal" proyectoTitulo={proCal} />
+
       </div>
       {/* Card */}
-      <div className="border rounded-2xl mx-40 flex flex-col md:flex-row justify-center items-center p-8">
-        <div className="w-7/12">
-        <h1>Title</h1>
-        <p className="text-red-400">IMG</p>
-        <p>Description</p>
-        </div>
-        <div className="w-5/12">
-          <p>Dificultad</p>
-          <div>
-            <p>Tecnololgias</p>
+      <div className="border-2 rounded-2xl flex flex-col justify-center items-center p-5 md:mx-40 mt-5">
+        <h1>{infoProyecto.proyectoNombre}</h1>
+        <div className="flex flex-col md:flex-row justify-center items-center border w-full">
+          <div className="w-7/12">
+            <p className="text-red-400">IMG</p>
+            <p>{infoProyecto.proyectoDescripcion}</p>
+            <button>
+              <a href={infoProyecto.proyectoDemo} target="_Blank">Demo</a>
+            </button>
+          </div>
+          <div className="w-5/12">
+            <p>Dificultad</p>
+            <div>
+              <p>Tecnologias</p>
+              <p>{infoProyecto.proyectoTecnologias}</p>
+            </div>
           </div>
         </div>
       </div>
